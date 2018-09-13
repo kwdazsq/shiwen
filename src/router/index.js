b0d845e0df3fd6917520f095e1737d795165d42f
kwdd <kongweidong@itjuzi.com>
2018-09-13 11:12:53 +0800

    初定整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Headers from '@/components/HomePage/Headers'
-import Aside from '@/components/HomePage/Aside'
-// import Individual from '@/components/HomePage/homepage/Individual'
+import Headers from '@/components/Frame/Headers'
+import Aside from '@/components/Frame/Aside'
+import Home from '@/components/HomePage/home'
+import Data from '@/components/HomePage/Details/Data'
 
 Vue.use(Router)
 
 export default new Router({
 	routes: [
-  // 		{
-		//       	path: '/HomePage',
-		//       	name: 'Headers',
-		//       	component: Headers
-		// },
 		{
-    		path:"/HomePage",
-    		name:"Aside",
-    		component:Aside,
-    		redirect:"/HomePage/homepage/Individual",
-    		// children:[
-	    	// 	{
-	    	// 		name:"Individual",
-	    	// 		path:"Individual",
-	    	// 		component:Individual
-	    	// 	},
-    		// ]
-    	},
-  	]
+			path:"/",
+			name:"home",
+			component:Home,
+			redirect:"/data",
+			children:[
+				{
+					path:"/data",
+					name:"首页",
+					components: {
+						default: Data,
+						headers: Headers,
+						aside: Aside
+					},
+				},
+			]
+		},
+		]
 })
